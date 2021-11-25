bbb48f7fd28e328f04f579041b66075e73c6f38f
roy70118 <mayankkumar27sept.mk@gmail.com>
2021-11-25 19:50:10 +0530

    Initial commit

---
+++ madlibs.py
@@ -0,0 +1,14 @@
+# madilo
+#  its a project for filling  a gaps of paragraph
+#  so , I m using string concatenate for filling the gaps of  paragraph
+
+
+adj = input("Adjective:")
+verb1 = input("verb: ")
+verb2 = input("verb: ")
+famous_person = input("famous person: ")
+
+madlib = f" computer programming is so {adj}! It makes me so excited all the time because \
+    I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
